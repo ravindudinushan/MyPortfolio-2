65a10ca448250fa2513d1888d26324e70dceddbd
Ravindu Dinushan <ravindudinushan9966@gmail.com>
2024-08-29 16:18:23 +0530

    tailwind css config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,9 +12,12 @@ const config = {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: "15px",
       screens: {
-        "2xl": "1400px",
+        sm: "640px",
+        md: "768px",
+        lg: "960px",
+        xl: "1200px",
       },
     },
     extend: {
