ed1062020c486c0d0c5e825e075ba52d7fa997fe
Ravindu Dinushan <ravindudinushan9966@gmail.com>
2024-08-29 16:53:50 +0530

    tailwind css config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,47 +13,22 @@ const config = {
     container: {
       center: true,
       padding: "15px",
+    },
       screens: {
         sm: "640px",
         md: "768px",
         lg: "960px",
         xl: "1200px",
       },
+    fontFamily: {
+      primary: "var(--font-jetbrainsMono)",
     },
     extend: {
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
+        primary: "#1c1c22",
         accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+          DEFAULT: "#00ff99",
+          hover: "#00e187",
         },
       },
       borderRadius: {
