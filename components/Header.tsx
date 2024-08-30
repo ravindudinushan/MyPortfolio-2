61fbbc64fed7440573ed3d84bfc8495f7f8d92d6
Ravindu Dinushan <ravindudinushan9966@gmail.com>
2024-08-30 07:11:53 +0530

    nav bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ import Nav from "./Nav";
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-        <div className="container mx-auto">
+        <div className="container mx-auto flex justify-between items-center">
             {/* Logo */}
             <Link href="/">
                 <h1 className="text-4xl font-semibold">
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,30 @@
+"use client";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "services",
+    path: "/services",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
 
 const Nav = () => {
   return (
